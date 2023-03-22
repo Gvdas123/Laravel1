16d733b2f4843b556407208e2e3b4034ee3e41e1
Gvidas123 <gvidasone@gmail.com>
2023-03-22 14:26:39 +0200

    Middleware

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -19,6 +19,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                    
                   
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __("Login") }}</a>
